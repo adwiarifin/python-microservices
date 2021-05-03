4cd42df42c69c1f9128154aa2097a4bb192e6114
Adwi Arifin <adwiarifin@gmail.com>
2021-05-04 00:11:30 +0700

    [FE] List & Delete Product

--- frontend/src/admin/Products.tsx
+++ frontend/src/admin/Products.tsx
@@ -1,31 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Product } from '../interfaces/Product';
 import Wrapper from './Wrapper';
 
 const Products = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('http://localhost:8000/api/products');
+            const data = await response.json();
+            setProducts(data);
+        })();
+    }, []);
+
+    const del = async (id: number) => {
+        if (window.confirm('Are you sure you want to delete this product?')) {
+            await fetch(`http://localhost:8000/api/products/${id}`, {
+                method: 'DELETE'
+            });
+            setProducts(products.filter((p: Product) => p.id !== id));
+        }
+    }
+
     return (
         <Wrapper>
             <div className="table-responsive">
-            <table className="table table-striped table-sm">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Header</th>
-                        <th>Header</th>
-                        <th>Header</th>
-                        <th>Header</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1,001</td>
-                        <td>random</td>
-                        <td>data</td>
-                        <td>placeholder</td>
-                        <td>text</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+                <table className="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Image</th>
+                            <th>Title</th>
+                            <th>Likes</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {products.map((p: Product) => {
+                        return (
+                            <tr key={p.id}>
+                                <td>{p.id}</td>
+                                <td><img src={p.image} height="180" /></td>
+                                <td>{p.title}</td>
+                                <td>{p.likes}</td>
+                                <td>
+                                    <div className="btn-group mr-2">
+                                        <a href="#" className="btn btn-sm btn-outline-secondary" onClick={() => del(p.id)}>Delete</a>
+                                    </div>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                        
+                    </tbody>
+                </table>
+            </div>
         </Wrapper>
     );
 };
