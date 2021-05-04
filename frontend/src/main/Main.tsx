c709a02779b15a15757af7edab2b43b606412882
Adwi Arifin <adwiarifin@gmail.com>
2021-05-04 12:03:54 +0700

    :art: formatting

--- frontend/src/main/Main.tsx
+++ frontend/src/main/Main.tsx
@@ -33,26 +33,24 @@ const Main = () => {
             <div className="album py-5 bg-light">
                 <div className="container">
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                        {products.map(
-                            (p: Product) => {
-                                return (
-                                    <div className="col" key={p.id}>
-                                        <div className="card shadow-sm">
-                                            <img src={p.image} alt={p.title} height="180" />
-                                            <div className="card-body">
-                                                <p className="card-text">{p.title}</p>
-                                                <div className="d-flex justify-content-between align-items-center">
-                                                    <div className="btn-group">
-                                                    <button type="button" className="btn btn-sm btn-outline-secondary"
-                                                            onClick={() => like(p.id)}>Like</button>
-                                                    </div>
-                                                    <small className="text-muted">{p.likes} likes</small>
+                        {products.map((p: Product) => {
+                            return (
+                                <div className="col" key={p.id}>
+                                    <div className="card shadow-sm">
+                                        <img src={p.image} alt={p.title} height="180" />
+                                        <div className="card-body">
+                                            <p className="card-text">{p.title}</p>
+                                            <div className="d-flex justify-content-between align-items-center">
+                                                <div className="btn-group">
+                                                <button type="button" className="btn btn-sm btn-outline-secondary"
+                                                        onClick={() => like(p.id)}>Like</button>
                                                 </div>
+                                                <small className="text-muted">{p.likes} likes</small>
                                             </div>
                                         </div>
                                     </div>
-                                )
-                            }
+                                </div>
+                            )}
                         )}
                     </div>
                 </div>
